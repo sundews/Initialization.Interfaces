8831adc69d9884d9894479870fa84006bf4629db
Kim Hugener-Ohlsen <kim@hugener.dk>
2023-04-30 00:37:18 +0200

    Added IInitializationReporter

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Initialization.Interfaces
 
-Package for introducing the IInitializable and IAsyncInitializable interface.
+Package providing the IInitializable, IAsyncInitializable and IInitializationReporter interfaces.
---
+++ Source/Initialization.Interfaces/IInitializationReporter.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IInitializationReporter.cs" company="Sundews">
+// Copyright (c) Sundews. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Initialization.Interfaces;
+
+/// <summary>
+/// Interface for implementing a <see cref="IInitializable"/> reporter.
+/// </summary>
+public interface IInitializationReporter
+{
+    /// <summary>
+    /// Called when an initializable has been initialized.
+    /// </summary>
+    /// <param name="source">The source.</param>
+    /// <param name="initializable">The initializable.</param>
+    void Initialized(object source, IInitializable initializable);
+
+    /// <summary>
+    /// Called when an async initializable has been initialized.
+    /// </summary>
+    /// <param name="source">The source.</param>
+    /// <param name="initializable">The initializable.</param>
+    void Initialized(object source, IAsyncInitializable initializable);
+}
